274fe1c032a31cf3eae66cb4bdbdd01555f67608
Arushi2010 <sujata2005paul@gmail.com>
2025-06-29 16:37:07 +0530

    acp project

---
+++ Lesson-23/after class project.js
@@ -0,0 +1,15 @@
+function first(){
+    console.log("I am first")
+}
+
+function second(){
+    console.log("I am second")
+}
+
+function third(){
+    console.log("I am third")
+}
+
+first()
+second()
+third()
---
+++ Lesson-23/project.js
@@ -0,0 +1,19 @@
+friends=["Shambhavi", "Mishikaa", "Arushi", "Eshanika" ]
+console.log(friends)
+
+console.log("The one good in maths :", friends[0])
+console.log("The one good in science :", friends[1])
+console.log("The one good in computer science :", friends[2])
+console.log("The one good in AI :", friends[3])
+
+friends[0]="Isha"
+console.log("updated array :", friends)
+
+const numbers=[1,2,3,4,5];
+console.log(numbers.lenght);
+
+numbers.push(6);
+console.log(numbers);
+
+numbers.pop()
+console.log(numbers);
